194bac71871c68d880cae243ba23439780c1266c
Gatilin Maxim <gatilin222@gmail.com>
2018-04-14 15:37:53 +0300

    Upgrade react-scripts and use css modules

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import styles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div className={styles.app}>App</div>
     );
   }
 }
