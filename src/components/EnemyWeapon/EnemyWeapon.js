aac0594ea27e6bb5fce6e503c734259b98c5276b
Gatilin Maxim <gatilin222@gmail.com>
2018-04-21 08:22:25 +0300

    Made enemy move

--- src/components/EnemyWeapon/EnemyWeapon.js
+++ src/components/EnemyWeapon/EnemyWeapon.js
@@ -2,12 +2,30 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import styles from './EnemyWeapon.module.css';
 import {ENEMY_WEAPON_HEIGHT} from '../../utils/constants';
+import {inject, observer} from 'mobx-react/index';
 
-
+@inject('EnemyWeaponStore')
+@observer
 class EnemyWeapon extends Component {
   static propTypes = {
     topPosition: PropTypes.number.isRequired
   };
+
+  componentDidMount() {
+    let direction = 'bottom';
+
+    const interval = setInterval(() => {
+      const currentPosition = this.props.topPosition;
+      if (currentPosition > 80 && direction === 'bottom') {
+        direction = 'top';
+      }
+      if (currentPosition < 20 && direction === 'top') {
+        direction = 'bottom';
+      }
+      const nextPosition = direction ==='top' ? currentPosition - 1 : currentPosition + 1;
+      this.props.EnemyWeaponStore.setTopPosition(nextPosition);
+    }, 20);
+  }
 
   render() {
     return (
