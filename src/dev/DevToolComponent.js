346b36f7e448fc1a9860132b85251238091b9610
Gatilin Maxim <gatilin222@gmail.com>
2018-04-22 11:07:46 +0300

    Add dev tool component for simulating

--- src/dev/DevToolComponent.js
+++ src/dev/DevToolComponent.js
@@ -2,17 +2,34 @@ import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react/index';
 
 @inject('GameStore')
+@inject('PlayerStore')
+@inject('EnemyStore')
 @observer
 class DevToolComponent extends Component {
   static propTypes = {};
 
+  damagePlayer = () => {
+    this.props.PlayerStore.damage();
+  };
+
+  damageEnemy = () => {
+    this.props.EnemyStore.damage();
+  };
+
   render() {
+    const buttonStyle = {
+      display: 'block',
+      margin: '10px 0 0'
+    };
     return (
       <div style={{
+        padding: 20,
         border: '1px solid black',
         margin: 20
       }}>
         Game state: {this.props.GameStore.state}
+        <button style={buttonStyle} onClick={this.damagePlayer}>Damage player</button>
+        <button style={buttonStyle} onClick={this.damageEnemy}>Damage enemy</button>
       </div>
     );
   }
